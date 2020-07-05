a221bade8a898c4b1a1728ac9ebc3afb7b63c17c
Jouda Hidri <hidrijouda@gmail.com>
2020-07-05 16:30:53 +0200

    Add spring data rest

--- src/main/java/com/codenotfound/kafka/SpringKafkaApplication.java
+++ src/main/java/com/codenotfound/kafka/SpringKafkaApplication.java
@@ -1,5 +1,6 @@
 package com.codenotfound.kafka;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/com/codenotfound/kafka/domain/MerchantRepo.java
@@ -0,0 +1,8 @@
+package com.codenotfound.kafka.domain;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+public interface MerchantRepo extends CrudRepository<Merchant, Long> {
+
+}
