2198990724a416f135658901f3bbae6b463593fd
Jouda Hidri <hidrijouda@gmail.com>
2020-07-08 00:24:45 +0200

    add sender to api

--- src/main/java/com/codenotfound/kafka/api/Setting.java
+++ src/main/java/com/codenotfound/kafka/api/Setting.java
@@ -20,4 +20,9 @@ public class Setting {
 	public void setName(String name) {
 		this.name = name;
 	}
+
+	@Override
+	public String toString() {
+		return id + ":" + name;
+	}
 }
--- src/main/java/com/codenotfound/kafka/api/SettingsController.java
+++ src/main/java/com/codenotfound/kafka/api/SettingsController.java
@@ -1,22 +1,25 @@
 package com.codenotfound.kafka.api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.codenotfound.kafka.producer.Sender;
+
 @RestController
 public class SettingsController {
-	
+
+	@Autowired
+	Sender sender;
+
 	@ResponseStatus(HttpStatus.CREATED)
 	@PostMapping("/settings")
 	public void create(@RequestBody Setting setting) {
-	}
-	
-	@ResponseStatus(HttpStatus.OK)
-	@GetMapping("/setting")
-	public void show() {
+		sender.send("boot.t", setting.toString());
+		//todo params: Boolean autocommit
 	}
 
 }
